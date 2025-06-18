c5ef5c019963dc840cc03213be8e80b4e628e1f9
Valter Sundström <valter.sundstrom@gmail.com>
2025-06-19 01:58:19 +0200

    Better macro syntax

--- lib/frix/macro.ex
+++ lib/frix/macro.ex
@@ -1,22 +1,31 @@
 defmodule Frix.Macro do
   @moduledoc """
-  Macros for writing DSL expressions with nicer syntax that compiles to C code.
+  Macros for writing DSL expressions with natural Elixir syntax that compiles to C code.
   """
 
   @doc """
-  Macro for building DSL with a nicer syntax.
+  Macro for building DSL with natural Elixir expressions.
   
   Example:
       c_program do
-        add_var(:count, :int, 42)
-        add_func(:add, :int, [x: :int, y: :int], ["x + y"])
+        var :count, :int, 42
+        var :max_size, :long, 1024
+        
+        function :add, :int, [x: :int, y: :int] do
+          return x + y
+        end
+        
+        function :main, :int do
+          printf("Hello World!")
+          return 0
+        end
       end
   """
   defmacro c_program(do: block) do
     quote do
-      import Frix.Macro
+      import Frix.Macro, only: [var: 3, function: 3, function: 4, return: 1]
       var!(dsl) = Frix.DSL.new()
-      unquote(block)
+      unquote(transform_block(block))
       Frix.DSL.generate_c_code(var!(dsl))
     end
   end
@@ -24,7 +33,7 @@ defmodule Frix.Macro do
   @doc """
   Adds a variable to the current DSL context.
   """
-  defmacro add_var(name, type, value) do
+  defmacro var(name, type, value) do
     quote do
       var!(dsl) = Frix.DSL.add_variable(var!(dsl), unquote(to_string(name)), unquote(to_string(type)), unquote(value))
     end
@@ -33,10 +42,10 @@ defmodule Frix.Macro do
   @doc """
   Adds a function with parameters to the current DSL context.
   """
-  defmacro add_func(name, return_type, params, body) do
+  defmacro function(name, return_type, params, do: body) do
     quote do
       params_list = unquote(build_params(params))
-      body_list = unquote(build_body_list(body))
+      body_list = unquote(transform_function_body(body))
       var!(dsl) = Frix.DSL.add_function(var!(dsl), unquote(to_string(name)), unquote(to_string(return_type)), params_list, body_list)
     end
   end
@@ -44,41 +53,62 @@ defmodule Frix.Macro do
   @doc """
   Adds a function without parameters to the current DSL context.
   """
-  defmacro add_func(name, return_type, body) do
+  defmacro function(name, return_type, do: body) do
     quote do
-      body_list = unquote(build_body_list(body))
+      body_list = unquote(transform_function_body(body))
       var!(dsl) = Frix.DSL.add_function(var!(dsl), unquote(to_string(name)), unquote(to_string(return_type)), [], body_list)
     end
   end
 
   @doc """
-  Helper macro for creating return statements.
+  Return statement for functions.
   """
-  defmacro ret(expr) do
+  defmacro return(_expr) do
+    # This is handled by transform_function_body - should not be called directly
+    raise "return/1 can only be used inside function bodies"
+  end
+
+  # Private helper functions for AST transformation
+
+  defp transform_block({:__block__, _, statements}) do
+    statements
+    |> Enum.map(&transform_statement/1)
+    |> Enum.reduce(quote(do: nil), fn stmt, acc ->
+      quote do
+        unquote(acc)
+        unquote(stmt)
+      end
+    end)
+  end
+
+  defp transform_block(single_statement) do
+    transform_statement(single_statement)
+  end
+
+  defp transform_statement({:var, _, [name, type, value]}) do
     quote do
-      Frix.DSL.return_stmt(unquote(expr))
+      var!(dsl) = Frix.DSL.add_variable(var!(dsl), unquote(to_string(name)), unquote(to_string(type)), unquote(value))
     end
   end
 
-  @doc """
-  Helper macro for creating assignments.
-  """
-  defmacro assign(var_name, expr) do
+  defp transform_statement({:function, _, [name, return_type, params, [do: body]]}) do
     quote do
-      Frix.DSL.assign(unquote(to_string(var_name)), unquote(expr))
+      params_list = unquote(build_params(params))
+      body_list = unquote(transform_function_body(body))
+      var!(dsl) = Frix.DSL.add_function(var!(dsl), unquote(to_string(name)), unquote(to_string(return_type)), params_list, body_list)
     end
   end
 
-  @doc """
-  Helper macro for creating function calls.
-  """
-  defmacro call(func_name, args \\ []) do
+  defp transform_statement({:function, _, [name, return_type, [do: body]]}) do
     quote do
-      Frix.DSL.call_function(unquote(to_string(func_name)), unquote(args))
+      body_list = unquote(transform_function_body(body))
+      var!(dsl) = Frix.DSL.add_function(var!(dsl), unquote(to_string(name)), unquote(to_string(return_type)), [], body_list)
     end
   end
 
-  # Private helper functions
+  defp transform_statement(stmt) do
+    stmt
+  end
 
   defp build_params(params) when is_list(params) do
     params
@@ -87,20 +117,75 @@ defmodule Frix.Macro do
     end)
   end
 
-  defp build_body_list(body_list) when is_list(body_list) do
-    body_list
-    |> Enum.map(fn
-      {:ret, expr} -> quote do: Frix.DSL.return_stmt(unquote(expr))
-      {:assign, var, expr} -> quote do: Frix.DSL.assign(unquote(to_string(var)), unquote(expr))
-      {:call, func, args} -> quote do: Frix.DSL.call_function(unquote(to_string(func)), unquote(args))
-      {:{}, _, [:ret, expr]} -> quote do: Frix.DSL.return_stmt(unquote(expr))
-      {:{}, _, [:assign, var, expr]} -> quote do: Frix.DSL.assign(unquote(to_string(var)), unquote(expr))
-      {:{}, _, [:call, func, args]} -> quote do: Frix.DSL.call_function(unquote(to_string(func)), unquote(args))
-      expr when is_binary(expr) -> quote do: Frix.DSL.return_stmt(unquote(expr))
-    end)
+  defp transform_function_body({:__block__, _, statements}) do
+    statements |> Enum.map(&transform_body_statement/1)
   end
 
-  defp build_body_list(single_expr) do
-    [quote do: Frix.DSL.return_stmt(unquote(single_expr))]
+  defp transform_function_body(single_statement) do
+    [transform_body_statement(single_statement)]
+  end
+
+  # Transform function body statements
+  defp transform_body_statement({:return, _, [expr]}) do
+    quote do: Frix.DSL.return_stmt(unquote(transform_expression(expr)))
+  end
+
+  defp transform_body_statement({:=, _, [{var_name, _, nil}, expr]}) when is_atom(var_name) do
+    quote do: Frix.DSL.assign(unquote(to_string(var_name)), unquote(transform_expression(expr)))
+  end
+
+  defp transform_body_statement({{:., _, [{func_name, _, nil}]}, _, args}) when is_atom(func_name) do
+    transformed_args = args |> Enum.map(&transform_expression/1)
+    quote do: Frix.DSL.call_function(unquote(to_string(func_name)), unquote(transformed_args))
+  end
+
+  defp transform_body_statement({func_name, _, args}) when is_atom(func_name) and is_list(args) do
+    transformed_args = args |> Enum.map(&transform_expression/1)
+    quote do: Frix.DSL.call_function(unquote(to_string(func_name)), unquote(transformed_args))
+  end
+
+  defp transform_body_statement({func_name, _, nil}) when is_atom(func_name) do
+    quote do: Frix.DSL.call_function(unquote(to_string(func_name)), [])
+  end
+
+  # Transform expressions to C-like strings
+  defp transform_expression({:+, _, [left, right]}) do
+    quote do
+      "#{unquote(transform_expression(left))} + #{unquote(transform_expression(right))}"
+    end
+  end
+
+  defp transform_expression({:-, _, [left, right]}) do
+    quote do
+      "#{unquote(transform_expression(left))} - #{unquote(transform_expression(right))}"
+    end
+  end
+
+  defp transform_expression({:*, _, [left, right]}) do
+    quote do
+      "#{unquote(transform_expression(left))} * #{unquote(transform_expression(right))}"
+    end
+  end
+
+  defp transform_expression({:/, _, [left, right]}) do
+    quote do
+      "#{unquote(transform_expression(left))} / #{unquote(transform_expression(right))}"
+    end
+  end
+
+  defp transform_expression({var_name, _, nil}) when is_atom(var_name) do
+    to_string(var_name)
+  end
+
+  defp transform_expression(literal) when is_integer(literal) do
+    literal
+  end
+
+  defp transform_expression(literal) when is_binary(literal) do
+    "\"#{literal}\""
+  end
+
+  defp transform_expression(expr) do
+    expr
   end
 end
--- test/frix/macro_test.exs
+++ test/frix/macro_test.exs
@@ -3,11 +3,11 @@ defmodule Frix.MacroTest do
   require Frix.Macro
   import Frix.Macro
 
-  describe "c_program macro" do
+  describe "c_program macro with natural syntax" do
     test "generates C code for simple program with variables" do
       c_code = c_program do
-        add_var(:count, :int, 42)
-        add_var(:max_size, :long, 1024)
+        var :count, :int, 42
+        var :max_size, :long, 1024
       end
 
       assert c_code =~ "int count = 42;"
@@ -16,7 +16,9 @@ defmodule Frix.MacroTest do
 
     test "generates C code for function without parameters" do
       c_code = c_program do
-        add_func(:get_answer, :int, "42")
+        function :get_answer, :int do
+          return 42
+        end
       end
 
       assert c_code =~ "int get_answer(void) {"
@@ -26,7 +28,9 @@ defmodule Frix.MacroTest do
 
     test "generates C code for function with parameters" do
       c_code = c_program do
-        add_func(:add, :int, [x: :int, y: :int], ["x + y"])
+        function :add, :int, [x: :int, y: :int] do
+          return x + y
+        end
       end
 
       assert c_code =~ "int add(int x, int y) {"
@@ -34,13 +38,13 @@ defmodule Frix.MacroTest do
       assert c_code =~ "}"
     end
 
-    test "generates C code for function with assignments and calls" do
+    test "generates C code for function with assignments" do
       c_code = c_program do
-        add_func(:calculate, :int, [
-          {:assign, :result, 10},
-          {:assign, :result, "result * 2"},
-          {:ret, "result"}
-        ])
+        function :calculate, :int do
+          result = 10
+          result = result * 2
+          return result
+        end
       end
 
       assert c_code =~ "int calculate(void) {"
@@ -50,12 +54,12 @@ defmodule Frix.MacroTest do
       assert c_code =~ "}"
     end
 
-    test "generates C code for function with calls" do
+    test "generates C code for function with function calls" do
       c_code = c_program do
-        add_func(:main, :int, [
-          {:call, :printf, ["\"Hello World!\""]},
-          {:ret, 0}
-        ])
+        function :main, :int do
+          printf("Hello World!")
+          return 0
+        end
       end
 
       assert c_code =~ "int main(void) {"
@@ -66,17 +70,17 @@ defmodule Frix.MacroTest do
 
     test "generates complete C program" do
       c_code = c_program do
-        add_var(:global_counter, :int, 0)
+        var :global_counter, :int, 0
         
-        add_func(:increment, :void, [
-          {:assign, :global_counter, "global_counter + 1"}
-        ])
+        function :increment, :void do
+          global_counter = global_counter + 1
+        end
         
-        add_func(:main, :int, [
-          {:call, :increment, []},
-          {:call, :printf, ["\"Counter: %d\\n\"", "global_counter"]},
-          {:ret, 0}
-        ])
+        function :main, :int do
+          increment()
+          printf("Counter: %d\\n", global_counter)
+          return 0
+        end
       end
 
       assert c_code =~ "int global_counter = 0;"
@@ -87,10 +91,40 @@ defmodule Frix.MacroTest do
       assert c_code =~ "printf(\"Counter: %d\\n\", global_counter);"
       assert c_code =~ "return 0;"
     end
+
+    test "handles arithmetic expressions" do
+      c_code = c_program do
+        function :math_ops, :int, [a: :int, b: :int] do
+          sum = a + b
+          diff = a - b
+          product = a * b
+          quotient = a / b
+          return sum + diff + product + quotient
+        end
+      end
+
+      assert c_code =~ "sum = a + b;"
+      assert c_code =~ "diff = a - b;"
+      assert c_code =~ "product = a * b;"
+      assert c_code =~ "quotient = a / b;"
+      assert c_code =~ "return sum + diff + product + quotient;"
+    end
+
+    test "handles function calls with multiple arguments" do
+      c_code = c_program do
+        function :test_calls, :void do
+          printf("Number: %d, String: %s", 42, "hello")
+          sprintf(buffer, "Value: %d", value)
+        end
+      end
+
+      assert c_code =~ "printf(\"Number: %d, String: %s\", 42, \"hello\");"
+      assert c_code =~ "sprintf(buffer, \"Value: %d\", value);"
+    end
   end
 
   describe "DSL helper functions" do
-    test "builds DSL manually with helper macros" do
+    test "builds DSL manually with helper functions" do
       import Frix.DSL
       
       dsl = 
@@ -105,14 +139,12 @@ defmodule Frix.MacroTest do
       assert c_code =~ "    return counter;"
     end
 
-    test "uses individual helper macros" do
-      result_ret = ret(42)
-      result_assign = assign(:x, "10")
-      result_call = call(:printf, ["hello"])
-
-      assert result_ret == Frix.DSL.return_stmt(42)
-      assert result_assign == Frix.DSL.assign("x", "10")
-      assert result_call == Frix.DSL.call_function("printf", ["hello"])
+    test "return macro outside function raises error" do
+      assert_raise RuntimeError, "return/1 can only be used inside function bodies", fn ->
+        quote do
+          return(42)
+        end |> Code.eval_quoted()
+      end
     end
   end
 end
